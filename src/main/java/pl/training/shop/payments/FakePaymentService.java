fb7af07ef1cc4d4afa7637147db0694c1b75e387
martazpk <martazpk@wp.pl>
2020-06-21 21:53:43 +0200

    UUIDPaymentIdGenerator extracted interface and given an interface instead of concrete impl. Created another impl - IncrementalPaymentIdGenerator

--- src/main/java/pl/training/shop/payments/FakePaymentService.java
+++ src/main/java/pl/training/shop/payments/FakePaymentService.java
@@ -1,13 +1,16 @@
 package pl.training.shop.payments;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
 import java.time.Instant;
 
 @Log
+@RequiredArgsConstructor
 public class FakePaymentService {
     private static final String LOG_FORMAT = "A new Payment of %s has been initialized";
-    private final UUIDPaymentIdGenerator generator = new UUIDPaymentIdGenerator();
+    private final PaymentIdGenerator generator;
+
     public Payment process(PaymentRequest request){
         var payment = Payment.builder()
                 .id(generator.getNext())
