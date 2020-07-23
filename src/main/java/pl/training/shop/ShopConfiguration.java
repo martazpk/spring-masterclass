e810b218f265722960c556de25817617e53438ea
martazpk <martazpk@wp.pl>
2020-07-23 22:53:44 +0200

    add hikari dependency (pooling) configuration source of connections to the database

--- src/main/java/pl/training/shop/ShopConfiguration.java
+++ src/main/java/pl/training/shop/ShopConfiguration.java
@@ -1,20 +1,36 @@
 package pl.training.shop;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 
+import javax.sql.DataSource;
+
+@PropertySource("classpath:jdbc.properties")
 @EnableAspectJAutoProxy
 @Configuration
 public class ShopConfiguration {
 
     @Bean
-    public MessageSource messageSource(){
+    public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    DataSource dataSource(Environment environment) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setUsername(environment.getProperty("database.username"));
+        dataSource.setPassword(environment.getProperty("database.password"));
+        dataSource.setJdbcUrl(environment.getProperty("database.url"));
+        dataSource.setDriverClassName(environment.getProperty("database.driver"));
+        return dataSource;
     }
 }
